e8b814879d44475ed69dd6004b76b56b353b0dd3
MaciejITT <maciej.wadas.it@gmail.com>
2020-07-27 19:05:09 +0200

    Base view

---
+++ bookmate_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BookmateAppConfig(AppConfig):
+    name = 'bookmate_app'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Lenovo\PycharmProjects\facebook_like_app_django\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
