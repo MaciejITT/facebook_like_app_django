3f4db1bb8337757ff917052445c2262b237da6d6
MaciejITT <maciej.wadas.it@gmail.com>
2020-07-27 20:30:05 +0200

    Basic views 2

--- bookmate_app/urls.py
+++ bookmate_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('login/', views.login_page, name='login'),
+    path('signin/', views.signin_page, name='signin'),
 ]
--- bookmate_app/views.py
+++ bookmate_app/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 
 def home_page(request):
     return render(request, 'base.html')
+
+
+def login_page(request):
+    return render(request, "bookmate/login.html")
+
+
+def signin_page(request):
+    form = UserCreationForm()
+    context ={
+        'form': form,
+    }
+    return render(request, "bookmate/signin.html", context)
 
